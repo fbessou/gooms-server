94819d921d758731ae13f7f832b76216a632745a
Frank Bessou <fbessou@free.fr>
2016-07-23 15:57:48 +0200

    Added states.

---
+++ src/game.ts
@@ -0,0 +1,4 @@
+export abstract class Game {
+	private name: string;
+	private states: GameState[];
+}
---
+++ src/gamestate.ts
@@ -0,0 +1,8 @@
+import { Game } from "./game";
+
+export abstract class GameState {
+	constructor(private game: Game) { }
+	enter : () => {};
+	leave : () => {};
+	update: () => {}
+}
